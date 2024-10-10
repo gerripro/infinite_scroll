b50c9bdbcb1bb0c4fe32d49bfa6897c117f6432e
gerripro <gerriprofessional@gmail.com>
2024-10-10 16:18:19 -0400

    web version 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2704658410"
+    serviceWorkerVersion: "1523190383"
   }
 });
